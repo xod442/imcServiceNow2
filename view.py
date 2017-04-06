2c3449970f6be4b4fced68537002ce6c88a5e6c9
Rick Kauffman <rick@rickkauffman.com>
2017-04-06 14:09:01 -0400

    added apache2 license to view.py

--- view.py
+++ view.py
@@ -1,3 +1,36 @@
+#!/usr/bin/env python
+'''
+  2017 wookieware.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+
+__author__ = "@netwookie"
+__copyright__ = "2017, wookieware.."
+__credits__ = ["Rick Kauffman"]
+__license__ = "Apache2"
+__version__ = "1.0.0"
+__maintainer__ = "Rick Kauffman"
+__email__ = "rick@rickkauffman.com"
+__status__ = "Prototype"
+
+Functions to interact with Sevice Now and snowBridge local databases
+03212017 Initial release.
+
+
+'''
+
+
 import time
 from flask import Flask, request, render_template, redirect, url_for, flash, session, send_file
 from flask.ext.bootstrap import Bootstrap
